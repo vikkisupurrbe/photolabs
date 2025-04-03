91a44ab450acc965bf4c59f0d9e94959234d44f0
vikkisupurrbe <yingjialiu1216@gmail.com>
2025-04-03 15:59:12 -0700

    Add HomeRoute to group nav bar and PhotoList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,12 @@
 import React, { useState } from 'react';
-import TopNavigationBar from './components/TopNavigationBar'
-import TopicList from './components/TopicList';
-import PhotoList from './components/PhotoList';
+import HomeRoute from './routes/HomeRoute'
 import './App.scss';
-
-// Sample data
-const sampleDataForPhotoListItem = {
-  id: 1,
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  urls: {
-    full: "/Image-1-Full.jpeg",
-    regular: "/Image-1-Regular.jpeg",
-  },
-  user: {
-    username: "exampleuser",
-    name: "Joe Example",
-    profile: "/profile-1.jpg",
-  },
-};
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopNavigationBar />
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,9 +1,13 @@
 import '../styles/HomeRoute.scss';
+import TopNavigationBar from '../components/TopNavigationBar';
+import PhotoList from '../components/PhotoList';
 
 const HomeRoute = () => {
   return (
     <div className="home-route">
       {/* Insert React */}
+      <TopNavigationBar />
+      <PhotoList />
     </div>
   );
 };
