302ac7fc64d16cb07a055b24699f2ee1246c4353
vikkisupurrbe <yingjialiu1216@gmail.com>
2025-04-02 19:49:05 -0700

    Apply styling to PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,17 +6,22 @@ const PhotoListItem = (props) => {
   const {location, urls, user} = props.photo;
   console.log(props) // Debugging to ensure props are passed correctly
   return(
-    <article className="photo-list-item">
-      <figure className="photo-list-item__image">
-        <img className="photo-list-item__photo" src={urls.regular} alt="Photo" />
-      </figure>
+    <div className="photo-list__item">
+      {/* Photo */}
+        <img className="photo-list__image" src={urls.regular} alt="Photo" />
+      
+      {/* Photographer info */}
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src= {user.profile} alt="Profile picture" />
 
-      <section className="photo-list-item__photographer">
-        <img className="photo-list-item__profile-pic" src= {user.profile} alt="Profile picture" />
-        <p className="photo-list-item__username">{user.username}</p>
-        <p className="photo-list-item__location">{location.city}, {location.country}</p>
-      </section>
-    </article>
+        <div className="photo-list__user-info">
+          <div>{user.username}</div>
+          <div className="photo-list-item__location">
+            {location.city}, {location.country}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 };
 
