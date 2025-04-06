34a8032ce49e51bc210c4474e573936851281051
vikkisupurrbe <yingjialiu1216@gmail.com>
2025-04-05 18:40:27 -0700

    Add larger version and photo info to the modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,7 +3,7 @@ import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = (props) => {
   const { switchModalOff, selectedPhoto } = props;
-  console.log(selectedPhoto);
+  console.log(props);
 
   // Handle click function
   const handleClick = () => {
@@ -12,9 +12,30 @@ const PhotoDetailsModal = (props) => {
 
   return (
     <div className="photo-details-modal">
+      {/* Close button */}
       <button onClick={handleClick} className="photo-details-modal__close-button">
         <img src={closeSymbol} alt="close symbol" />
       </button>
+
+      {/* Wrapper for the image and photographer info */}
+      <div className="photo-details-modal__images">
+        {/* Larger version of the selected photo and its info */}
+        <img className="photo-details-modal__image" src={selectedPhoto.urls.full} alt="Larger photo" />
+        
+        {/* Photographer details */}
+        <div className="photo-details-modal__photographer-details">
+          <img className="photo-details-modal__photographer-profile" src={selectedPhoto.user.profile} alt="Profile picture" />
+          <div className="photo-details-modal__photographer-info">
+            <div>{selectedPhoto.user.username}</div>
+            <div className="photo-details-modal__photographer-location">
+              {selectedPhoto.location.city}, {selectedPhoto.location.country}
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
     </div>
   )
 };
