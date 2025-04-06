1ca84d8ac8b54a1fd03c34e63c593d00e9bb8bbd
vikkisupurrbe <yingjialiu1216@gmail.com>
2025-04-06 13:11:16 -0700

    Implement useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,32 +1,53 @@
-import { useState } from "react";
-import { toggleFav, switchModalOn, switchModalOff } from "../helpers/photoHelpers";
+import { useReducer } from "react";
+
+// Initial state of the app
+const initialState = {
+  fav: {}, // Favorite photo IDs
+  selectedPhoto: null, // Currently selected photo (for modal)
+};
+
+// Reducer function
+function reducer(state, action) {
+  switch (action.type) {
+    case "TOGGLE_FAV":
+      return {
+        ...state,
+        fav: {
+          ...state.fav,
+          [action.photoId]: !state.fav[action.photoId]
+        }
+      };
+    case "SELECT_PHOTO":
+      return {
+        ...state,
+        selectedPhoto: action.photo
+      };
+    case "CLOSE_MODAL":
+      return {
+        ...state,
+        selectedPhoto: null
+      };
+    default:
+      return state;
+  }
+}
 
 const useApplicationData = () => {
-  // Define state for fav button
-  const [fav, setFav] = useState({});
-
-  // Define the state for selected photo (for modal)
-  const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   // Action: update favorite photo
   const updateToFavPhotoIds = (photoId) => {
-    toggleFav(photoId, setFav);
+    dispatch({ type: "TOGGLE_FAV", photoId });
   };
 
   // Action: select a photo to open modal
   const setPhotoSelected = (photo) => {
-    switchModalOn(photo, setSelectedPhoto);
+    dispatch({ type: "SELECT_PHOTO", photo });
   };
 
   // Action: close the modal
   const onClosePhotoDetailsModal = () => {
-    switchModalOff(setSelectedPhoto);
-  };
-
-  // State object to return
-  const state = {
-    fav,
-    selectedPhoto
+    dispatch({ type: "CLOSE_MODAL" });
   };
 
   return {
